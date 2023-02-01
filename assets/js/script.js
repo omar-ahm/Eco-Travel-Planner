db5ff889e67312b528f57000bf3191e71866afee
Omar-ahm <irycevier@gmail.com>
2023-02-01 21:15:32 +0000

    js updated with initial pseudocode and README updated with initial proposal plan

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Pseudocode for Eco Travel App
+
+// 1. Accept input from user for location, dates, and number of travellers.
+var searchDestination = $('#dest-input');
+var tripStart = $('#start-date-input');
+var tripEnd = $('#end-date-input');
+var numOfTravellers = $('#num-of-travellers-input');
+var carbOffsetProg = $('carbon-Os-Prog-input');
+var results = $('#results');
+
+// 2. Use Google Maps API to search for eco-friendly accommodations in the selected location.
+
+// 3. Display a list of accommodations to the user, including trip date, photos, description, price, and reviews.
+
+// 4. Use API or data source to retrieve information on sustainable transportation options (public transportation, car rentals, carbon offset programs).
+
+// 5. Sort the transportation options by relevance.
+
+// 6. Display detailed information on each transportation option to the user, including cost, availability, and reviews.
+
+// 7. Calculate the estimated carbon emissions for the trip.
+
+// 8. Present the user with a list of carbon offset programs.
+
+// 9. Accept input from user on the selected carbon offset program.
+
+// 10. Record the carbon offset in the user's account.
