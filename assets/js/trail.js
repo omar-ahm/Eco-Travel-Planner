1f99188203b67120b79b5db64e5386c7d16b1559
HelloSeva <sevda.seyidova611@gmail.com>
2023-02-06 14:16:53 +0000

    TrailAPI added, links in navbar fixed

--- assets/js/trail.js
+++ assets/js/trail.js
@@ -1,68 +1,115 @@
 
+// Google Maps and Trail Api Key
+apiKey = "AIzaSyANek184qE1iZOIHlPpUs_Nk34G_AA2DT4";
+trailApi = "df0bec8614msh80779c66b5b858ap1b8aa9jsn05a4fe08c52a";
 
-//global variables
-var mainElement = document.querySelector("#content")
-var cityName = document.querySelector("#city-input")
-var sumbitElement = document.querySelector("#submit-button")
-var locationArray = [];
-var trailLocations = []
+let userInput = document.getElementById('userInput');
+let searchButton = document.getElementById('searchButton');
+let searchResults = document.getElementById('searchResults');
+let spinnerContainer = document.getElementById('spinner-container');
+let mapsContainer = document.getElementById('maps-container');
 
-
-//api call to get lon/lat using the users inputed city
-function getLocation(event) {
-    var citySearch = cityName.value
-    var apiKey = 'c930bf6f68b3d8e517378bd63f068cbb'
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=imperial`
-
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            locationArray.push(new Location(data.coord.lat, data.coord.lon))
-            trailApi()
-        });
-}
-
-//creates location object to store lat/lon
-function Location(lat, lon) {
-    this.lat = lat;
-    this.lon = lon;
-}
-
-
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
-});
-
-//api call to get trail data based on lat/lon
-async function trailApi(event) {
-    fetch(`https://trailapi-trailapi.p.rapidapi.com/trails/explore/&lon=${locationArray[0].lon}&limit=25&lat=${locationArray[0].lat}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "df0bec8614msh80779c66b5b858ap1b8aa9jsn05a4fe08c52a",
-            "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
+// function initMap() {
+function initMap() {
+    var options = {
+        types: ['(cities)'],
+        componentRestrictions: {
+            country: ["us", "gb"]
         }
-    })
-        .then(function (response) {
-            return response.json();
+    };
+    var input = document.getElementById('userInput');
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+}
+
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    runSearch();
+})
+userInput.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    if (e.keyCode === 13) {
+        document.getElementById('searchButton').click()
+        runSearch();
+    }
+})
+
+// Start the function and fetch
+function runSearch() {
+    spinnerContainer.style.display = "block"
+
+    let inputText = userInput.value
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${inputText}&key=${apiKey}`)
+        .then(response => response.json())
+        .then(latData => {
+            let lat = latData.results[0].geometry.location.lat;
+            let lng = latData.results[0].geometry.location.lng;
+            return fetch(`https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=${lat}&lon=${lng}`,
+                {
+                    "method": "GET",
+                    "headers": {
+                        "X-RapidAPI-Host": "trailapi-trailapi.p.rapidapi.com",
+                        "X-RapidAPI-Key": `${trailApi}`
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.data);
+                    let html = data.data.map(trailData => {
+                        while (searchResults.firstChild) {
+                            searchResults.removeChild(searchResults.firstChild);
+                        };
+                        if (trailData.thumbnail === null) {
+                            trailData.thumbnail = "assets/images/slider-1.jpeg";
+                        };
+                        if (trailData.rating === 0) {
+                            trailData.rating = 'no rating';
+                        }
+                        if (trailData.difficulty === '') {
+                            trailData.difficulty = 'unknown';
+                        };
+                        if (trailData.length === '0.0') {
+                            trailData.length = 'unknown'
+                        }
+                        return `<div class="card" id="searchResults">
+                <img src="${trailData.thumbnail}" class="card-img-top" alt="trail image">
+                <div class="card-body" >
+                  <h5 class="card-title">${trailData.name}</h5>
+                  <p>City: ${trailData.city}</p>
+                  <p class="difficulty">Difficulty: ${trailData.difficulty}</p>
+                  <p class="length">Length: ${trailData.length} miles</p>
+                  <p id="rateMe2"  class="empty-stars">Rating: ${trailData.rating}</p>
+                  <a href="${trailData.url}" target="_blank" rel="noopener noreferrer" id="details-button" class="details-button btn btn-info mt-1">Detail</a>
+                  <button id="mapsButton" class="maps-button btn btn-info mt-1" value="${trailData.lat},${trailData.lon}">Map</button>
+                </div>
+              </div>`
+                    }).join('');
+                    document.querySelector('#searchResults').insertAdjacentHTML('afterbegin', html);
+                    userInput.value = '';
+
+                    spinnerContainer.style.display = "none"
+                    location.href = '#searchResults'
+
+                    let mapsButton = document.querySelectorAll('#mapsButton');
+                    console.log(mapsButton, 60);
+
+                    mapsButton.forEach(mapsButton => {
+                        mapsButton.addEventListener('click', (e) => {
+                            console.log(mapsButton.value);
+                            mapsContainer = document.querySelector('#map-container')
+                            mapsContainer.innerHTML = ''
+                            let iframe = `
+                    <iframe style="border:0"
+                    loading="lazy"
+                                allowfullscreen
+                                src='https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${mapsButton.value}
+                                &zoom=18
+                                &maptype=satellite'>
+                                </iframe>`
+                            console.log(iframe, 75);
+                            mapsContainer.innerHTML = iframe;
+                            location.href = '#map-container';
+                        })
+                    })
+                })
         })
-        .then(response => {
-            trailLocations = []
-            for (let i = 0; i < response.places.length; i++) {
-                if (response.places[i].description !== null) {
-                    trailLocations.push({ name: response.places[i].name, description: response.places[i].description, lat: response.places[i].lat, lon: response.places[i].lon })
-                }
-            }
-            render()
-        })
-
-
-        .catch(err => {
-            console.error(err);
-        });
-
-
 }
